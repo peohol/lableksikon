b801012a71c6680db077328873889440abe00d6c
Peder Holman <162774417+peohol@users.noreply.github.com>
2026-09-17 10:07:29 +0200

    Publiser hele kategorien Kvalitet i måling

--- src/content/terms/index.ts
+++ src/content/terms/index.ts
@@ -1,8 +1,23 @@
 import type { PublishedTerm } from "../schema";
 
 import { presisjon } from "./presisjon";
-import { maleusikkerhet } from "./maleusikkerhet";
+import { repeterbarhet } from "./repeterbarhet";
+import { intermediar } from "./intermediar";
+import { reproduserbarhet } from "./reproduserbarhet";
 import { noyaktighet } from "./noyaktighet";
+import { skjevhet } from "./skjevhet";
+import { gjenvinning } from "./gjenvinning";
+import { robusthet } from "./robusthet";
+import { selektivitet } from "./selektivitet";
+import { spesifisitet } from "./spesifisitet";
+import { sporbarhet } from "./sporbarhet";
+import { validering } from "./validering";
+import { verifisering } from "./verifisering";
+import { maleusikkerhet } from "./maleusikkerhet";
+import { utvidet } from "./utvidet";
+import { dekningsfaktor } from "./dekningsfaktor";
+import { kontrollkort } from "./kontrollkort";
+import { folsomhet } from "./folsomhet";
 import { linearitet } from "./linearitet";
 import { deteksjonsgrense } from "./deteksjonsgrense";
 import { internstandard } from "./internstandard";
@@ -22,8 +37,23 @@ import { opplosning } from "./opplosning";
 export const publishedTerms: PublishedTerm[] = [
   // Kvalitet i måling
   presisjon,
-  maleusikkerhet,
+  repeterbarhet,
+  intermediar,
+  reproduserbarhet,
   noyaktighet,
+  skjevhet,
+  gjenvinning,
+  robusthet,
+  selektivitet,
+  spesifisitet,
+  sporbarhet,
+  validering,
+  verifisering,
+  maleusikkerhet,
+  utvidet,
+  dekningsfaktor,
+  kontrollkort,
+  folsomhet,
   // Kalibrering og kontroll
   linearitet,
   deteksjonsgrense,
