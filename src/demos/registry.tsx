ff8513798a938f4fc8ade31f3f94777b98e29325
Peder Holman <162774417+peohol@users.noreply.github.com>
2026-09-17 08:53:18 +0200

    Rett lint i demonstrasjonsregisteret

--- src/demos/registry.tsx
+++ src/demos/registry.tsx
@@ -2,29 +2,72 @@ import dynamic from "next/dynamic";
 
 import type { DemoId } from "./ids";
 
-const qualityDemo = <T extends keyof typeof import("./QualityConceptDemos")>(name: T) =>
-  dynamic(() => import("./QualityConceptDemos").then((module) => module[name]));
+const RepeterbarhetDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.RepeterbarhetDemo),
+);
+const IntermediarDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.IntermediarDemo),
+);
+const ReproduserbarhetDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.ReproduserbarhetDemo),
+);
+const SkjevhetDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.SkjevhetDemo),
+);
+const GjenvinningDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.GjenvinningDemo),
+);
+const UtvidetUsikkerhetDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.UtvidetUsikkerhetDemo),
+);
+const DekningsfaktorDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.DekningsfaktorDemo),
+);
+const SelektivitetDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.SelektivitetDemo),
+);
+const SpesifisitetDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.SpesifisitetDemo),
+);
+const FolsomhetDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.FolsomhetDemo),
+);
+const RobusthetDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.RobusthetDemo),
+);
+const SporbarhetDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.SporbarhetDemo),
+);
+const ValideringDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.ValideringDemo),
+);
+const VerifiseringDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.VerifiseringDemo),
+);
+const KontrollkortDemo = dynamic(() =>
+  import("./QualityConceptDemos").then((module) => module.KontrollkortDemo),
+);
 
 /** Kobler et begrep til sin demonstrasjon. */
 const demos = {
   "presisjon-spredning": dynamic(() => import("./PresisjonSpredning")),
-  "repeterbarhet-serie": qualityDemo("RepeterbarhetDemo"),
-  "intermediar-forhold": qualityDemo("IntermediarDemo"),
-  "reproduserbarhet-lab": qualityDemo("ReproduserbarhetDemo"),
+  "repeterbarhet-serie": RepeterbarhetDemo,
+  "intermediar-forhold": IntermediarDemo,
+  "reproduserbarhet-lab": ReproduserbarhetDemo,
   "riktighet-skiver": dynamic(() => import("./RiktighetSkiver")),
-  "skjevhet-referanse": qualityDemo("SkjevhetDemo"),
-  "gjenvinning-spike": qualityDemo("GjenvinningDemo"),
+  "skjevhet-referanse": SkjevhetDemo,
+  "gjenvinning-spike": GjenvinningDemo,
   usikkerhetsbudsjett: dynamic(() => import("./Usikkerhetsbudsjett")),
-  "utvidet-intervall": qualityDemo("UtvidetUsikkerhetDemo"),
-  "dekningsfaktor-k": qualityDemo("DekningsfaktorDemo"),
-  "selektivitet-interferens": qualityDemo("SelektivitetDemo"),
-  "spesifisitet-terminologi": qualityDemo("SpesifisitetDemo"),
-  "folsomhet-stigning": qualityDemo("FolsomhetDemo"),
-  "robusthet-variasjon": qualityDemo("RobusthetDemo"),
-  "sporbarhet-kjede": qualityDemo("SporbarhetDemo"),
-  "validering-formal": qualityDemo("ValideringDemo"),
-  "verifisering-krav": qualityDemo("VerifiseringDemo"),
-  "kontrollkort-serie": qualityDemo("KontrollkortDemo"),
+  "utvidet-intervall": UtvidetUsikkerhetDemo,
+  "dekningsfaktor-k": DekningsfaktorDemo,
+  "selektivitet-interferens": SelektivitetDemo,
+  "spesifisitet-terminologi": SpesifisitetDemo,
+  "folsomhet-stigning": FolsomhetDemo,
+  "robusthet-variasjon": RobusthetDemo,
+  "sporbarhet-kjede": SporbarhetDemo,
+  "validering-formal": ValideringDemo,
+  "verifisering-krav": VerifiseringDemo,
+  "kontrollkort-serie": KontrollkortDemo,
   "linearitet-kurve": dynamic(() => import("./LinearitetKurve")),
   "deteksjonsgrense-stoy": dynamic(() => import("./DeteksjonsgrenseStoy")),
   "internstandard-forhold": dynamic(() => import("./InternstandardForhold")),
